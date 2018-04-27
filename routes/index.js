7e0c312fca2ca253a384728418e28840150dd4cb
alexthemonk <lius214@wfu.edu>
2018-04-26 20:13:00 -0400

    data base totally online now. looks promising

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 //The landing points
+var tree = require('../models/tree');
 
 var express = require('express');
 var router = express.Router();
@@ -7,8 +8,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	//when get(/), do function()
 	//Then the server gives back the information to the web browser
-	res.render('home',
-		{title: "Wake Forest Tree Map"});
+
+	tree.find({}, 'tree_label longitude latitude')
+	.populate()
+  .exec(function (err, list_trees) {
+    if (err) { return next(err); }
+    //Successful, so render
+		list_trees = JSON.stringify(list_trees);
+		list_trees = JSON.parse(list_trees);
+		res.render('home', {title: "Wake Forest Tree Map", tree_list: list_trees});
+  });
 });
 
 module.exports = router;
